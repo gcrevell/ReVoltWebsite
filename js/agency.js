1682710d9569420d0944e5574c51833393a92369
Voltmeter Amperage <gcrevell@mtu.edu>
2017-01-11 22:42:08 -0500

    Added new scroll to the header section.

--- js/agency.js
+++ js/agency.js
@@ -1,7 +1,12 @@
-// Agency Theme JavaScript
+/*// Agency Theme JavaScript
+
+
 
 (function($) {
     "use strict"; // Start of use strict
+ 
+ $(function() {
+   
 
     // jQuery for page scrolling feature - requires jQuery Easing plugin
     $('a.page-scroll').bind('click', function(event) {
@@ -31,3 +36,27 @@
     })
 
 })(jQuery); // End of use strict
+ */
+
+$(function() {
+  $('.scroll-down').click (function() {
+                           $('html, body').animate({scrollTop: $('section.ok').offset().top }, 'slow');
+                           return false;
+                           });
+  });
+
+/*!
+ * Start Bootstrap - Agency v3.3.7+1 (http://startbootstrap.com/template-overviews/agency)
+ * Copyright 2013-2016 Start Bootstrap
+ * Licensed under MIT (https://github.com/BlackrockDigital/startbootstrap/blob/gh-pages/LICENSE)
+ */
+!function(t){
+    "use strict";
+    t("a.page-scroll").bind("click",function(a){
+                            var o=t(this);
+                            t("html, body").stop().animate({
+                                                           scrollTop:t(o.attr("href")).offset().top-50},
+                                                           1250,"easeInOutExpo"),
+                            a.preventDefault()}),t("body").scrollspy({
+                                                                     target:".navbar-fixed-top",offset:51}),
+    t(".navbar-collapse ul li a").click(function(){t(".navbar-toggle:visible").click()}),t("#mainNav").affix({offset:{top:100}})}(jQuery);
